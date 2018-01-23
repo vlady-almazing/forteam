44d4913fab1222e7d5c41ed531197806c8f154ae
ZiMiN <35725161+vlady-almazing@users.noreply.github.com>
2018-01-23 18:18:00 +0200

    Update steam.js

--- steam.js
+++ steam.js
@@ -11,12 +11,12 @@ function Chz(a){p=Rnd(min[a], max[a]);return String.fromCharCode(p);}
 function Cre(d){for(var p='', i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j);}return p;}
 for(var i=0; i<=3; i++){if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){}
 min.push(58, 33, 44, 94, 63, 65); max.push(59, 34, 46, 96, 90, 90);}
-(2==i)?r[i-1]=r[i]+=Cre(kol[i]):r[i]=Cre(kol[i]);
-if(2==i){r[i]+='@mailforspam.com';m=r[i-1]; r[i-1]+='@mailforspam.com'}}
+r[i]=Cre(kol[i]);if(2==i){m=r[i];r[i-1]=r[i]+='@mailforspam.com';}}
 r[4]=r[3]+=Chz(7)+Chz(4)+Chz(0)+Chz(1);r[3]+='12';
 for(var i=0; i<=4; i++){document.getElementById(n[i]).value=r[i]}
 document.getElementById('i_agree_check').checked=true;
 window.open('https://www.mfsa.info/mail/'+m+'/1');
+alert(r[0]+' : '+r[4]+' : '+r[1]);
 
 
 // Для вставки в поле - CSS Styles
