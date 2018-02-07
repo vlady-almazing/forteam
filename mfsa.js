d2f24e13082f1865fe7932827f37769bfa6a38dd
ZiMiN <35725161+vlady-almazing@users.noreply.github.com>
2018-02-07 02:36:49 +0200

    Update mfsa.js

--- mfsa.js
+++ mfsa.js
@@ -4,6 +4,6 @@
 
 // Для вставки в поле - JS
 var t='', n=0, v=1, j=0, z=500, st='https://store.steampowered.com/account/newaccountverification?';
-function Ie(){j++; if(j>=4) location.reload(); if(v){v=0; setInterval(Ie, z);}}e=document.getElementById('messagebody');
-!e&&Ie();t=document.getElementById('messagebody').textContent;n=t.indexOf('stoken');t=st+t.substr(n, 137);
-window.open(t);window.close()
+function re(){j++; if(j>=6) location.reload(); if(v){v=0; setInterval(re, z);}}e=document.getElementById('messagebody');
+!e&&re();t=document.getElementById('messagebody').textContent;n=t.indexOf('stoken');t=st+t.substr(n, 137);
+window.open(t);window.close();
